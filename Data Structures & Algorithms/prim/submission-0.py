f6667d63181e114b7db4bdd14e0bfd0bb3372fd9
RohanBarshan <rohandeb2006@gmail.com>
2026-07-08 16:59:07 -0700

    Add: prim - submission-0

---
+++ Data Structures & Algorithms/prim/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: List[List[int]]) -> int:
+        adj = {}
+        for i in range(n):
+            adj[i] = []
+
+        for n1, n2, weight in edges:
+            adj[n1].append((n2, weight))
+            adj[n2].append((n1, weight))
+        
+        minHeap = [[0,0]]
+        res = 0
+        visit = set()
+        while minHeap and len(visit) < n:
+            weight, v = heapq.heappop(minHeap)
+
+            if v in visit:
+                continue
+            
+            res += weight
+            visit.add(v)
+
+            for neighbor, weight in adj[v]:
+                if neighbor not in visit:
+                    heapq.heappush(minHeap, [weight, neighbor])
+
+        return res if len(visit) == n else - 1
