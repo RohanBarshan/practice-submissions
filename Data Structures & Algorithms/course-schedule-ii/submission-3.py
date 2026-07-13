c9d474b8e27f2d67dfb2171952e1c92ef3e594db
RohanBarshan <rohandeb2006@gmail.com>
2026-07-12 22:03:43 -0700

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+        for dst, src in prerequisites:
+            adj[src].append(dst)
+        topSort = []
+        visiting = set()
+        visited = set()
+
+        def dfs(src):
+            if src in visited:
+                return True
+            if src in visiting:
+                return False
+            
+            visiting.add(src)
+            for neighbor in adj[src]:
+                if not dfs(neighbor):
+                    return False
+            visiting.remove(src)
+            visited.add(src)
+            topSort.append(src)
+            return True
+            
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        topSort.reverse()
+        return topSort
