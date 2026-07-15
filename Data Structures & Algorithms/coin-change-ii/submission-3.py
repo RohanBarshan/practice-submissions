e85109c7e87c4f9808858ec1ab50b0394e09df02
RohanBarshan <rohandeb2006@gmail.com>
2026-07-14 23:44:24 -0700

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for i in range(len(coins)-1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                #count the ways to get the amount if we skip the current coin
+                nextDP[a] = dp[a]
+                # check if we can use the current coin
+                if a - coins[i] >= 0:
+                    # Use one current coin, then add the ways to make the remaining amount.
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
